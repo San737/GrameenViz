1e8d11eb847ce5695d813e7051342b798e222414
Ayush Acharya <[avinashaacharya1@gmail.com]>
2024-03-21 10:24:23 +0530

    again

--- src/app/map_query/page.tsx
+++ src/app/map_query/page.tsx
@@ -31,7 +31,7 @@ export default function MapPage() {
 
   return (
     <div className="bg-slate-50">
-      <header className="absolute inset-x-0 top-0 z-50 bg-slate-400">
+      <header className="absolute inset-x-0 top-0 z-50 bg-slate-50">
         <nav
           className="flex items-center justify-between p-6 lg:px-8"
           aria-label="Global"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function homePage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center overflow-y-hidden bg-slate-50">
-      <header className="absolute inset-x-0 top-0 z-50 bg-slate-400">
+      <header className="absolute inset-x-0 top-0 z-50 bg-slate-50">
         <nav
           className="flex items-center justify-between p-6 lg:px-8"
           aria-label="Global"
@@ -68,12 +68,12 @@ export default function homePage() {
               >
                 Query the dataset
               </Link>
-              <Link
+              {/* <Link
                 href="/map_population"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Population Map
-              </Link>
+              </Link> */}
               <a
                 href="#"
                 className="text-sm font-semibold leading-6 text-gray-900"
